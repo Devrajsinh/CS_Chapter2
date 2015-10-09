e60e8ff5c5deac4e999ef24e6a97e30060436ffc
Devrajsinh <devrajsinh14@gmail.com>
2015-10-09 11:20:27 -0400

    eggs

--- CS_Chapter2/Eggs/Program.cs
+++ CS_Chapter2/Eggs/Program.cs
@@ -9,6 +9,14 @@ namespace Eggs
     {
         static void Main(string[] args)
         {
+            int egg1 = 19;
+            int egg2 = 12;
+            int egg3 = 23;
+            int egg4 = 28;
+            
+            int all = (egg1 + egg2 + egg3 + egg4);
+
+            Console.WriteLine("{0} eggs is equals to {1} dozens and {2} eggs", all, all /12, all % 12);
 
         }
     }
