d49c7b7e4829d864cc23e5479951a079039211e4
Devrajsinh <devrajsinh14@gmail.com>
2015-10-09 11:35:39 -0400

    eggsinter

--- CS_Chapter2/EggsInteractive/Program.cs
+++ CS_Chapter2/EggsInteractive/Program.cs
@@ -9,6 +9,16 @@ namespace EggsInteractive
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the first integer");
+            firstString = Console.ReadLine();
+            first = Convert.ToInt32(firstString);
+
+            Console.WriteLine("Enter the second integer");
+            secondString = Console.ReadLine();
+            second = Convert.ToInt32(secondString);
+
+            sum = first + second;
+            Console.WriteLine ("{0}, the sum of {1} and {2} is {3}", first, second, sum; 
         }
     }
 }
